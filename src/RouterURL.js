d239370ef2aa8eccb28cf1779e4e7b0cf14b21fa
RomelLopez <romelanthonylopez@gmail.com>
2022-06-15 14:38:00 -0700

    making website shown fixed route

--- src/RouterURL.js
+++ src/RouterURL.js
@@ -8,7 +8,7 @@ export default class routerURL extends Component {
     render() {
         return (
             <Routes>
-                <Route path="/" element={<Homepage />} />
+                <Route path={process.env.PUBLIC_URL} element={<Homepage />} />
                 <Route path="/NonAlcoholic" element={<NonAlcoholic />} />
                 <Route path='/Alcoholic' element={<Alcoholic />} />
                 <Route path='/Surprise' element={<Surprise />} />
